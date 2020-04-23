e95d4d7cd42e7d90c4b5f6c22b305888151fb65c
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-04-23 02:06:40 -0700

    Updated packages Added rc-slider customizations (to displace mark text rotated) Using datetimes as marks

--- src/App.js
+++ src/App.js
@@ -4,25 +4,41 @@ import './App.css';
 
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import './rc-slider-custom.css';
 
 function App() {
-  const Range = Slider.Range;
+  const dates = [
+      '2020-04-23T04:04:30.644Z',
+      '2020-04-23T04:24:30.644Z',
+      '2020-04-23T04:28:30.644Z',
+      '2020-04-23T04:44:30.644Z',
+      '2020-04-23T05:24:30.644Z',
+      '2020-04-23T05:28:30.644Z',
+      '2020-04-23T05:31:30.644Z',
+      '2020-04-23T05:37:36.635Z',
+      '2020-04-23T05:40:22.135Z',
+  ]
 
-  const marks = {
-    1: '1',
-    20: '2',
-    40: '40',
-    200: '200',
-    400: '400'
-  };
-
-  function log(value) {
-    console.log(value);
-  }
+  let marks = {};
+  dates.forEach(function(d){
+      marks[(new Date(d)).getTime()] = d;
+  })
+  const marksMin = Math.min(...Object.keys(marks));
+  const marksMax = Math.max(...Object.keys(marks));
 
   return (
     <div className="App">
-      <Range allowCross={false} marks={marks} onChange={log} />
+      <div style={{ marginLeft: '64px', marginRight: '64px', paddingTop: '64px' }}>
+        <Slider.Range
+          allowCross={false}
+          marks={marks}
+          step={null}
+          min={marksMin}
+          max={marksMax}
+          defaultValue={[marksMax,marksMax]}
+          onChange={console.log}
+        />
+      </div>
     </div>
   );
 }
