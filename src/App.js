efb478b5e2f5d30b077be3e58ea9a2c8e61057c8
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-04-23 19:46:42 -0700

    Created Timeline component that incorportes rc-slider. Added Bootstrap CDN css.

--- src/App.js
+++ src/App.js
@@ -2,43 +2,28 @@ import React from 'react';
 
 import './App.css';
 
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
-import './rc-slider-custom.css';
+import Timeline from './Timeline.js';
 
 function App() {
   const dates = [
-      '2020-04-23T04:04:30.644Z',
-      '2020-04-23T04:24:30.644Z',
-      '2020-04-23T04:28:30.644Z',
-      '2020-04-23T04:44:30.644Z',
-      '2020-04-23T05:24:30.644Z',
-      '2020-04-23T05:28:30.644Z',
-      '2020-04-23T05:31:30.644Z',
-      '2020-04-23T05:37:36.635Z',
-      '2020-04-23T05:40:22.135Z',
+    '2020-04-23T03:04:30.644Z',
+    '2020-04-23T03:14:30.644Z',
+    '2020-04-23T03:28:30.644Z',
+    '2020-04-23T03:44:30.644Z',
+    '2020-04-23T04:24:30.644Z',
+    '2020-04-23T04:28:30.644Z',
+    '2020-04-23T04:31:30.644Z',
+    '2020-04-23T04:37:36.635Z',
+    '2020-04-23T04:50:22.135Z',
+    '2020-04-23T05:13:22.135Z',
+    '2020-04-23T05:45:22.135Z',
   ]
-
-  let marks = {};
-  dates.forEach(function(d){
-      marks[(new Date(d)).getTime()] = d;
-  })
-  const marksMin = Math.min(...Object.keys(marks));
-  const marksMax = Math.max(...Object.keys(marks));
 
   return (
     <div className="App">
-      <div style={{ marginLeft: '64px', marginRight: '64px', paddingTop: '64px' }}>
-        <Slider.Range
-          allowCross={false}
-          marks={marks}
-          step={null}
-          min={marksMin}
-          max={marksMax}
-          defaultValue={[marksMax,marksMax]}
-          onChange={console.log}
-        />
-      </div>
+      <Timeline
+        dates={dates}
+      />
     </div>
   );
 }
---
+++ src/Timeline.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+import './Timeline.css';
+
+function Timeline(props) {
+  const dates = props.dates || []
+
+  let marks = {};
+  dates.forEach(function(d){
+      marks[(new Date(d)).getTime()] = d;
+  })
+  const marksMin = Math.min(...Object.keys(marks));
+  const marksMax = Math.max(...Object.keys(marks));
+
+  return (
+    <div className="Timeline">
+      <Slider.Range
+        allowCross={false}
+        marks={marks}
+        step={null}
+        min={marksMin}
+        max={marksMax}
+        defaultValue={[marksMax,marksMax]}
+        onChange={console.log}
+      />
+    </div>
+  );
+}
+
+export default Timeline;
