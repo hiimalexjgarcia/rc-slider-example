01ed784b1f1296ebcb8ee20108370316f1bf49c9
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-04-29 11:50:57 -0700

    Saving dates in ISO format (for later conversion to UNIX time) Added margins to buttons Increased rotation of slider mark text Refactored some code

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 import React, {useState} from 'react';
-import Timeline from './Timeline.js';
+import './App.css';
+import Timeline from './Timeline';
 import Button from 'react-bootstrap/Button';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 
-const style = {margin: 10}
 function App() {
-    const [dates, setDates] = useState([]);
+  const [dates, setDates] = useState([]);
 
-    const handleClick = () => {
-        setDates(dates.concat(new Date().toLocaleString()))
-    };
+  const handleClick = (e) => {
+    console.log(e.target.value);
+    setDates([
+      ...dates,
+      new Date().toISOString()
+    ]);
+  }
 
-    const [displayType, setDisplayType] = useState('select')
-    const handleChange = (selectedValue) => {
-        setDisplayType(selectedValue);
-    };
-    
-    //gotta add different handlers for different color semscreens (this would mock up how semscreens owned by different colored hats would show up)
-    return (
-        <>
-            <Button id="button" style={style} onClick={handleClick}>Add red semscreen</Button>
-            <Button id="button" style={style} onClick={handleClick}>Add green semscreen</Button>
-            <Button id="button" style={style} onClick={handleClick}>Add blue semscreen</Button>
-            <ToggleButtonGroup type="radio" name="select-filter" defaultValue={'select'} onChange={handleChange}>
-                <ToggleButton variant="secondary" value={'select'}>Select</ToggleButton>
-                <ToggleButton variant="secondary" value={'filter'}>Filter</ToggleButton>
-            </ToggleButtonGroup>
-            <Timeline
-                dates={dates}
-                displayType={displayType}
-            />
-        </>
-    );
+  const [displayType, setDisplayType] = useState('select')
+
+  const handleChange = (selectedValue) => {
+    setDisplayType(selectedValue);
+  };
+
+  //gotta add different handlers for different color semscreens (this would mock up how semscreens owned by different colored hats would show up)
+  return (
+    <div className="App">
+      <Button className="mr-2" onClick={handleClick} value="Red"  >Add red semscreen</Button>
+      <Button className="mr-2" onClick={handleClick} value="Green">Add green semscreen</Button>
+      <Button className="mr-2" onClick={handleClick} value="Blue" >Add blue semscreen</Button>
+      <ToggleButtonGroup type="radio" name="select-filter" defaultValue={'select'} onChange={handleChange}>
+        <ToggleButton variant="secondary" value={'select'}>Select</ToggleButton>
+        <ToggleButton variant="secondary" value={'filter'}>Filter</ToggleButton>
+      </ToggleButtonGroup>
+      <Timeline dates={dates} displayType={displayType} />
+    </div>
+  );
 }
 
 export default App;
--- src/Timeline.js
+++ src/Timeline.js
@@ -1,57 +1,48 @@
 import React from 'react';
+import './Timeline.css';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import './Timeline.css';
-import 'rc-slider/assets/index.css';
 
-const createSliderWithTooltip = Slider.createSliderWithTooltip;
-const Range = createSliderWithTooltip(Slider.Range);
+const Range = Slider.createSliderWithTooltip(Slider.Range);
 
 function Timeline(props) {
-    const dates = props.dates || []
+  const dates = props.dates || []
 
-    let marks = {};
-    dates.forEach(function(d){
-        marks[(new Date(d)).getTime()] = d;
-    })
+  let marks = {};
+  dates.forEach(function(d){
+    marks[(new Date(d)).getTime()] = d;
+  })
+  const marksMin = Math.min(...Object.keys(marks));
+  const marksMax = Math.max(...Object.keys(marks));
 
-    const marksMin = Math.min(...Object.keys(marks));
-    const marksMax = Math.max(...Object.keys(marks));
-    const width = (new Date().getTime() - marksMin)/300 || 0;
+  const displayType = props.displayType || 'select'
 
-    const style = { maxWidth: 400, margin: 35 };
+  const output = displayType === 'select' ?
+    <Slider
+      marks={marks}
+      step={null}
+      min={marksMin}
+      max={marksMax}
+      defaultValue={marksMax}
+      onAfterChange={console.log}
+    />
+  :
+    <Range
+      allowCross={false}
+      marks={marks}
+      step={null}
+      min={marksMin}
+      max={marksMax}
+      defaultValue={[marksMax,marksMax]}
+      pushable={true}
+      onAfterChange={console.log}
+    />
 
-    const displayType = props.displayType;
+  return (
+    <div className="Timeline">
+      { output }
+    </div>
+  );
+}
 
-    if (displayType === 'select') {
-        return (
-            <div style={style} className="timeline">
-                <Slider
-                    allowCross={false}
-                    marks={marks}
-                    step={null}
-                    min={marksMin}
-                    max={marksMax}
-                    defaultValue={[marksMax,marksMax]}
-                    pushable={true}
-                />
-            </div>
-        );
-    } else if (displayType === 'filter') {
-        return (
-            <div style={style} className="timeline">
-                <Range
-                    allowCross={false}
-                    marks={marks}
-                    step={null}
-                    min={marksMin}
-                    max={marksMax}
-                    defaultValue={[marksMax,marksMax]}
-                    pushable={true}
-                />
-            </div>
-        );
-    };
-};
-
-    export default Timeline;
+export default Timeline;
